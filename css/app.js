93c1933e547a2422c0a021e71f6795c8d19e89c6
charlie alton <charlie_alton123@hotmail.com>
2021-07-17 22:30:35 -0600

    Update app.js

--- css/app.js
+++ css/app.js
@@ -1,3 +1,13 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const startButton = document.querySelector('.btn__reset');
+
+startButton.addEventListener('click', (event) => {
+    const startOverlay = event.target.parentNode,
+        oldLetters = document.qurerySelector('letter')
+    if (startOverlay.className === 'start' || startOverlay.className === 'win' || startOverlay.className === 'lose') {
+        restart();
+        startOverlay.style.display = 'none';
+    }
+
+});
