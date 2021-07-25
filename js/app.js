c04c0f83bb2f532df2b892223a805d911ded0aaa
charlie alton <charlie_alton123@hotmail.com>
2021-07-24 22:52:20 -0600

    update js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const phrase = document.querySelector('#phrase');
 const startButton = document.querySelector('.btn__reset');
 
 startButton.addEventListener('click', (event) => {
-    const startOverlay = event.target.parentNode,
+    const startOverlay = event.target.parentNode;
     if (startOverlay.className === 'start' || startOverlay.className === 'win' || startOverlay.className === 'lose') {
         startOverlay.style.display = 'none';
     };
@@ -12,4 +12,38 @@ startButton.addEventListener('click', (event) => {
         // This is for when I declare functions and parameters.
     };
 
-});
+    phrases = [
+        'Coding is fun',
+        'JavaScript is lit',
+        'Your doing great.',
+        'Bull frogs and strange stuff',
+        'Keep learning',
+    ];
+
+    const phaseArray = getRandomPhraseArray(phrases);
+    addPhraseToDisplay(phraseArray);
+
+    const checkLetter = (letter) => {
+        let letters = document.querySelectorAll('.letter')
+        matchedLetterCount = 0;
+        letters.forEach(item => {
+            let currentLetter = item.innerHTML.toLowercase();
+            if (currentLetter === letter) {
+                item.className += 'show';
+                matchedLetterCount += 1;
+            }
+        });
+        if (matchedLetterCount === 0) {
+
+            letterFound = null;
+            matchedLetterCount = 0;
+        } else if (matchedLetterCount > 0) {
+            letterFound = letter;
+            matchedLetterCount = 0;
+        }
+    };
+
+
+
+
+
