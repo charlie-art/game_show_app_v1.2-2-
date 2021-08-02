5b50b9d7a06f49235d96fe4c349003198822a965
charlie alton <charlie_alton123@hotmail.com>
2021-08-01 22:16:33 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -23,22 +23,52 @@ startButton.addEventListener('click'), (event) => {
     const phrase = getRandomPhrase(phrases);
     addPhraseToDisplay(phrase);
 
-    const checkLetter = (letter) => {
-        let letters = document.querySelectorAll('.letter')
-        matchedLetterCount = 0;
-        letters.forEach(item => {
-            let currentLetter = item.innerHTML.toLowercase();
-            if (currentLetter === letter) {
-                item.className += 'show';
-                matchedLetterCount += 1;
-            }
-        });
-        if (matchedLetterCount === 0) {
+    qwerty.addEventListener('click', (click) => {
+        if (event.target.tagName === "BUTTON") {
+            let button,
+                letterFound = 'missed',
+                tries = document.querySelectorAll('.tries');
 
-            letterFound = null;
-            matchedLetterCount = 0;
-        } else if (matchedLetterCount > 0) {
-            letterFound = letter;
-            matchedLetterCount = 0;
         };
+
+        const checkLetter = (letter) => {
+            let letters = document.querySelectorAll('.letter')
+            matchedLetterCount = 0;
+            letters.forEach(item => {
+                let currentLetter = item.innerHTML.toLowercase();
+                if (currentLetter === letter) {
+                    item.className += 'show';
+                    matchedLetterCount += 1;
+                }
+            });
+            if (matchedLetterCount === 0) {
+
+                letterFound = null;
+                matchedLetterCount = 0;
+            } else if (matchedLetterCount > 0) {
+                letterFound = letter;
+                matchedLetterCount = 0;
+            };
+
+            checkWin = () => {
+                const revealledLetters = document.querySelectorAll('show'),
+                    lettersInPhase = document.querySelectorAll('.letter');
+                let startOverlay = document.querySelector('#overlay');
+                overlayTitle = document.querySelector('.title');
+
+                if (revealledLetters.length === lettersInPhase.length) {
+                    startOverlay.style.display = 'flex';
+                    startOverlay.classname = 'win';
+                    overlayTitle.textContent = 'You win';
+                }
+                else if (missed === 5) {
+                    startOverlay.style.display = 'flex';
+                    startOverlay.className = 'lose';
+                    overLayTitle.textContent = 'You Lose!';
+
+                }
+            };
+
+
+
 
