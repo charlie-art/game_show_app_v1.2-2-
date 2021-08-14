3308a1e89b218c5baa14fbf0f02313cf1e3274e0
charlie alton <charlie_alton123@hotmail.com>
2021-08-14 14:31:50 -0600

    js update

--- js/app.js
+++ js/app.js
@@ -29,66 +29,67 @@ function getRandomPhraseAsArray(arr) {
     return splitPhase
 };
 
-getRandomPhraseAsArray(phrases);
 
 
-addPhraseToDisplay(arr) {
+function addPhraseToDisplay(arr) {
     //Create a reference to ul element
     const myList = document.getElementById('myList');
 
     //Crete new list items
     let newListItem = document.createElement('ol');
     newListItem.textContent = (this.phrases);
+};
+
+addPharse(phrases);
 
 
 
-
-    const checkLetter = (letter) => {
-        let letters = document.querySelectorAll('.letter')
-        matchedLetterCount = 0;
-        letters.forEach(item => {
-            let currentLetter = item.innerHTML.toLowercase();
-            if (currentLetter === letter) {
-                item.className += 'show';
-                matchedLetterCount += 1;
-            }
-        });
-        if (matchedLetterCount === 0) {
-            letterFound = null;
-            matchedLetterCount = 0;
-        } else if (matchedLetterCount > 0) {
-            letterFound = letter;
-            matchedLetterCount = 0;
+const checkLetter = (letter) => {
+    let letters = document.querySelectorAll('.letter')
+    matchedLetterCount = 0;
+    letters.forEach(item => {
+        let currentLetter = item.innerHTML.toLowercase();
+        if (currentLetter === letter) {
+            item.className += 'show';
+            matchedLetterCount += 1;
         }
-    };
+    });
+    if (matchedLetterCount === 0) {
+        letterFound = null;
+        matchedLetterCount = 0;
+    } else if (matchedLetterCount > 0) {
+        letterFound = letter;
+        matchedLetterCount = 0;
+    }
+};
 
-//qwerty.addEventListener('click', (event) => {
-   // if (event.target.tagName === "BUTTON") {
-       // let button,
-         //   letterFound = 'missed',
-         //   tries = document.querySelectorAll('.tries');
+qwerty.addEventListener('click', (event) => {
+    if (event.target.tagName === "BUTTON") {
+        let button,
+            letterFound = 'missed',
+            tries = document.querySelectorAll('.tries');
 
-   // }
-//});
+    }
+});
 
-//checkWin = () => {
-   // const revealledLetters = document.querySelectorAll('show'),
-      //  lettersInPhase = document.querySelectorAll('.letter');
-   // let startOverlay = document.querySelector('#overlay');
-   // overlayTitle = document.querySelector('.title');
+checkWin = () => {
+    const revealledLetters = document.querySelectorAll('show'),
+        lettersInPhase = document.querySelectorAll('.letter');
+    let startOverlay = document.querySelector('#overlay');
+    overlayTitle = document.querySelector('.title');
 
-   // if (revealledLetters.length === lettersInPhase.length) {
-     //   startOverlay.style.display = 'flex';
-      //  startOverlay.className = 'win';
-      //  overlayTitle.textContent = 'You win';
-  //  }
-  //  else if (missed === 5) {
-     //   startOverlay.style.display = 'flex';
-     //   startOverlay.className = 'lose';
-     //   overLayTitle.textContent = 'You Lose!';
+    if (revealledLetters.length === lettersInPhase.length) {
+        startOverlay.style.display = 'flex';
+        startOverlay.className = 'win';
+        overlayTitle.textContent = 'You win';
+    }
+    else if (missed === 5) {
+        startOverlay.style.display = 'flex';
+        startOverlay.className = 'lose';
+        overLayTitle.textContent = 'You Lose!';
 
-  //  }
-//};
+    }
+};
 
 
 
