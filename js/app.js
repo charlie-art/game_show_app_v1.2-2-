c63948408259937407d95d22d9d16afec8e334df
charlie alton <charlie_alton123@hotmail.com>
2021-09-14 16:22:34 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const btn__reset = document.getElementsByClassName('btn__reset');
+const btn__reset = document.querySelector('.btn__reset');
+const missed = ('');
