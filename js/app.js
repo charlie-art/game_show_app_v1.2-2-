3a542af5a06da41275fc2cb45377a571dba0c886
charlie alton <charlie_alton123@hotmail.com>
2021-09-15 15:44:21 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const btn__reset = document.querySelector('.btn__reset');
-const missed = ('');
+let missed = ('0');
+
+phrases = {
+    'Coding Rules'
+'I love to code'
+'Fast Coding'
+'Index.Html'
+'Sass'
+
+
+
+};
