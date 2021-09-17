1fbd415055e1cfb3bfa84a28ece457e5029e0625
charlie alton <charlie_alton123@hotmail.com>
2021-09-17 15:50:16 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,29 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const btn__reset = document.querySelector('.btn__reset');
-let missed = ('0');
+let missed = 0;
 
 let phrases = [
-    ["Coding Rules",
-        "I love to code",
-        "Fast Coding",
-        "Index.Html",
-        "Sass",]
+    "Coding Rules",
+    "I love to code",
+    "Fast Coding",
+    "Index.Html",
+    "Sass",
 
 ];
 
-startGame.addEventListner('click', e => {
+btn__reset.addEventListener('click', e => {
 
 });
+
+btn__reset.style {
+    display: block;
+
+};
+
+btn__reset.style {
+    display: none;
+
+};
 
 
