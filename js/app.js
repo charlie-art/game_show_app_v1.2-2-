567615c7fdd90a5c20ef6137c480ee3151c42f2b
charlie alton <charlie_alton123@hotmail.com>
2021-09-25 13:16:09 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const btn__reset = document.querySelector('.btn__reset');
+
 let missed = 0;
 
 let phrase = [
@@ -12,14 +13,15 @@ let phrase = [
 
 ];
 
+
 btn__reset.addEventListener('click', e => {
     document.getElementById("overlay").style.display = "none";
 });
 
 function getRandomPhraseAsArray(arr) {
     // this is for random phrases to be selected.
-    var chosenPhrase = phrase[Math.floor(Math.random() * phrase.length)];
-    var splitPhrase = phrase.split(phrase);
+    var splitPhrases = phrase[Math.floor(Math.random() * arr.length)];
+    var chosenPhrase = phrase.split("");
 
     return splitPhrase;
 };
