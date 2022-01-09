12878dd20b5cb027f1e446aebee60931700a1529
charlie alton <charlie_alton123@hotmail.com>
2022-01-09 02:03:58 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -47,7 +47,7 @@ function addPhraseToDisplay(arr) {
 
         //Put the charater inside the list item.
 
-        li.arr[i] = "ul";
+        li.arr[i] = "li";
 
 
         //Append that list item to the #​ phrase u​ l in your HTML//
